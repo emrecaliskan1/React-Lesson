6694069a0a4169db7dcfa0ab8afae98458ac2607
Emre Çalışkan <114865537+emrecaliskan1@users.noreply.github.com>
2025-02-20 22:42:25 +0300

    week2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ExpenseItem from './components/ExpenseItem'
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return <div>
-    <ExpenseItem></ExpenseItem>
-    <ExpenseItem></ExpenseItem>
-  </div> 
+  
+  const expenses = [
+    {id:'a1',title:'Car Insurance',amount:294.67,date:new Date(2021,2,28)},
+    {id:'a2',title:'Toilet Paper',amount:67.12,date:new Date(2023,2,14)},
+    {id:'a3',title:'Cinema',amount:15,date:new Date(2025,1,1)},
+    {id:'a4',title:'McDonalds',amount:31.67,date:new Date(2022,7,8)}
+  ];
+
+
+
+  return (
+    <div>
+      <ExpenseItem
+        // id={expenses[0].id}
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        // id={expenses[1].id}
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}>
+      </ExpenseItem>
+
+      <ExpenseItem
+        // id={expenses[2].id}
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        // id={expenses[3].id}
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItem>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,9 +1,30 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem(){
+function ExpenseItem({date,amount,title}) {
 
-    return <h2 style={{color:"red"}}>
-        Expense Item !</h2>
+  const month = date.toLocaleDateString("en-US",{month:"long"});
+  const year = date.getFullYear();
+  const day = date.toLocaleDateString("en-US",{day:"2-digit"});
+
+  // console.log(props);
+  // const expenseDate = new Date(2021,2,28);
+  // const expenseTitle = 'Car Insurance';
+  // const expenseAmount = Math.random()*1000;
+  
+
+  return (
+    <div className="expense-item">
+      <div className="calendar">
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+        </div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount.toFixed(2)}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
